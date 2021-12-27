6dc74ce4f7c2e48d5236a1ad4899eca477c8d3f2
marinajulia <marinajulia707@gmail.com>
2021-12-27 09:07:46 -0300

    feat: ensure email validator adpter returns false if validator returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,6 @@
+import { EmailValidator } from '../presentation/protocols/email-validator'
+export class EmailValidatorAdapter {
+    isValid (email: string):  boolean{
+        return false
+    }
+}
