10e1b94c10d450344f86f4716ba5b5658bc889e1
marinajulia <marinajulia707@gmail.com>
2021-12-28 14:45:30 -0300

    feat: ensure dbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,17 +1,21 @@
-import { AddAccount, AddAccountModel, AccountModel, Encrypter} from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
     private readonly encrypter: Encrypter
-    constructor(encrypter) {
+    private readonly addAccountRepository: AddAccountRepository
+    constructor(encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
         this.encrypter = encrypter
+        this.addAccountRepository = addAccountRepository
     }
-    async add(account: AddAccountModel): Promise<AccountModel> {
-        this.encrypter.encrypt(account.password)
+    async add(accountData: AddAccountModel): Promise<AccountModel> {
+        const hashedPassword =  await this.encrypter.encrypt(accountData.password)
+        await this.addAccountRepository.add(Object.assign({}, accountData, {password: hashedPassword}))
+        //retorno provisório que coloquei pois não dava para retornar nulo
         return new Promise(resolve => resolve({
             id: '1',
-            name: account.name,
-            password: account.password,
-            email: account.email
+            name: accountData.name,
+            password: hashedPassword,
+            email: accountData.email
         }))
     }
 }
